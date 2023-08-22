2ec9dce87df9e3f2db0ec1295b65881b826e3e72
yan-s-h <127477303+yan-s-h@users.noreply.github.com>
2023-08-22 17:19:17 +0000

    Deploying to gh-pages from @ yan-s-h/project-front@96c300802f09159170448755f858e42edd79db2d 🚀

--- assets/BackButton-5d5178cf.js
+++
@@ -1,1 +0,0 @@
-import{am as o,Q as a,R as c,c as s,T as r,a1 as n,K as _}from"./index-fdaf6647.js";const u={class:"d-flex justify-center my-5"},B={__name:"BackButton",setup(l){const e=o(),t=()=>{e.go(-1)};return(d,i)=>(a(),c("div",u,[s(_,{onClick:t,color:"secondary"},{default:r(()=>[n("Back")]),_:1})]))}};export{B as _};
