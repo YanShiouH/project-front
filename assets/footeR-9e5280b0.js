44b528c656e9af95c8b8e7883f6b91bc1ac78a14
yan-s-h <127477303+yan-s-h@users.noreply.github.com>
2023-08-23 08:32:35 +0000

    Deploying to gh-pages from @ yan-s-h/project-front@a06615e0f6b6c11282d84faea9c40ae637af9842 🚀

---
+++ assets/footeR-9e5280b0.js
@@ -0,0 +1,1 @@
+import{ai as o,Q as s,R as t,ah as e}from"./index-312db973.js";const _={},r={class:"footer"},c=e("div",{class:"footer-content"},[e("p",null,"© 2023 Taealam. All rights reserved.")],-1),a=[c];function n(l,i){return s(),t("footer",r,a)}const f=o(_,[["render",n]]);export{f as F};
