1a2c24c2e0ba7abc1c5407349d20f7c6b08fa39f
yan-s-h <arena702048@gmail.com>
2023-10-14 22:45:39 +0800

    right to left

--- src/components/CourseContent.vue
+++ src/components/CourseContent.vue
@@ -2,7 +2,8 @@
   <v-card class="card">
     <v-card-title>
       <!-- {{ content }} -->
-      <div v-html="props.content" class="title-card"></div>
+      <div v-html="props.content.split('<br>')[0]" class="title-card-arabic" dir="rtl"></div>
+      <div v-html="props.content.split('<br>')[1]" class="title-card" dir="ltr"></div>
     </v-card-title>
     <v-card-actions>
       <v-btn icon="mdi-volume-medium" color="primary" @click="startSpeaking"></v-btn>
@@ -25,6 +26,7 @@ audio.src = new URL('/tts/' + props.content.split('<br>')[0], import.meta.env.VI
 const startSpeaking = async () => {
   audio.play()
 }
+
 </script>
 
 <style scoped lang="sass" src=".././assets/components/_courseContent.sass"></style>
